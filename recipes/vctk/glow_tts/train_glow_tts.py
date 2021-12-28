99e3b18b77c59c94c207f4c3a0fcb3e06765c322
ruohoruotsi <ruoho.ruotsi@gmail.com>
2021-12-27 17:18:16 -0800

    [ADD] open-bible-yo formattter and mods to glowtts trainer

--- recipes/vctk/glow_tts/train_glow_tts.py
+++ recipes/vctk/glow_tts/train_glow_tts.py
@@ -8,23 +8,46 @@ from TTS.tts.datasets import load_tts_samples
 from TTS.tts.models.glow_tts import GlowTTS
 from TTS.tts.utils.speakers import SpeakerManager
 from TTS.utils.audio import AudioProcessor
+from pathlib import Path
 
 # set experiment paths
 output_path = os.path.dirname(os.path.abspath(__file__))
-dataset_path = os.path.join(output_path, "../VCTK/")
+dataset_path = os.path.join(output_path, "/Users/rebeiro/github/coqui-tts/open-bible-yo/")
 
 # download the dataset if not downloaded
-if not os.path.exists(dataset_path):
-    from TTS.utils.downloaders import download_vctk
-
-    download_vctk(dataset_path)
+# if not os.path.exists(dataset_path):
+#     from TTS.utils.downloaders import download_vctk
+#
+#     download_vctk(dataset_path)
 
 # define dataset config
-dataset_config = BaseDatasetConfig(name="vctk", meta_file_train="", path=dataset_path)
+dataset_config = BaseDatasetConfig(name="open-bible-yo", meta_file_train="metadata.tsv", path=dataset_path)
 
 # define audio config
 # ❗ resample the dataset externally using `TTS/bin/resample.py` and set `resample=False` for faster training
-audio_config = BaseAudioConfig(sample_rate=22050, resample=True, do_trim_silence=True, trim_db=23.0)
+audio_config = BaseAudioConfig(sample_rate=48000, resample=False, do_trim_silence=True, trim_db=23.0)
+
+
+def bible_formatter(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
+    """Normalizes Open Bible dataset to TTS format"""
+    txt_file = os.path.join(root_path, meta_file)
+    speaker_name = Path(root_path).stem
+    items = []
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            cols = line.split("\t")
+            wav_file = os.path.join(root_path + "wavs", cols[0].strip())
+            text = cols[1]
+            if os.path.isfile(wav_file):
+                if len(text) > 0:
+                    text = text.strip()
+                    items.append([text, wav_file, speaker_name])
+                else:
+                    print("text len <= 0, empty text?")
+            else:
+                print("> File %s does not exist!" % (wav_file))
+    return items
+
 
 # define model config
 config = GlowTTSConfig(
@@ -41,6 +64,7 @@ config = GlowTTSConfig(
     phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
     print_step=25,
     print_eval=False,
+    audio=audio_config,
     mixed_precision=True,
     output_path=output_path,
     datasets=[dataset_config],
@@ -51,7 +75,8 @@ config = GlowTTSConfig(
 ap = AudioProcessor(**config.audio.to_dict())
 
 # load training samples
-train_samples, eval_samples = load_tts_samples(dataset_config, eval_split=True)
+train_samples, eval_samples = load_tts_samples(dataset_config, eval_split=True,
+                                               formatter=bible_formatter)
 
 # init speaker manager for multi-speaker training
 # it maps speaker-id to speaker-name in the model and data-loader
